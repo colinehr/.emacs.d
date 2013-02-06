f6b92ab6988bc29ded82d96f3008ee2d8998c051
Colin Ehr <cehr@lclark.edu>
2013-02-06 01:47:17 -0800

    First commit

---
+++ init.el
@@ -0,0 +1,92 @@
+;; Emacs configuration file
+;; this is under construction
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; APPEARANCE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Remove scroll, tool and menu bars
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;; Theme
+(load-theme 'tango-dark t)
+
+;; Font
+(set-default-font "DejaVu Sans Mono-8")
+
+;; Window size
+(add-to-list 'default-frame-alist '(height . 50))
+(add-to-list 'default-frame-alist '(width . 100))
+
+(show-paren-mode t)
+
+(setq inhibit-splash-screen t)
+(setq use-dialog-box "n")
+
+(require 'cc-mode)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook 'turn-on-font-lock)
+
+(require 'linum)
+(setq linum-format "%d")
+(global-linum-mode 1)
+
+;; backup settings and version control
+(setq backup-by-copying t
+      backup-directory-alist '(("." . "~/.emacs.d/saves"))
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
+;; make tab a tab
+;(setq c-default-style "k&r")
+(setq-default c-basic-offset 4)
+;(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+;(define-key c-mode-base-map (kbd "C-j") 'newline)
+(electric-indent-mode t)
+
+;; line by line scrolling
+(setq scroll-step 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGES
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar gnu '("gnu" . "http://elpa.gnu.org/packages/"))
+(defvar marmalade '("marmalade" . "http://marmalade-repo.org/packages/"))
+(defvar melpa '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(setq package-archives (list gnu marmalade melpa))
+
+(package-initialize)
+
+(unless (and (file-exists-p "~/.emacs.d/elpa/archives/marmalade")
+             (file-exists-p "~/.emacs.d/elpa/archives/gnu")
+             (file-exists-p "~/.emacs.d/elpa/archives/melpa"))
+  (package-refresh-contents))
+
+(defun packages-install (&rest packages)
+  (mapc (lambda (package)
+          (let ((name (car package))
+                (repo (cdr package)))
+            (when (not (package-installed-p name))
+              (let ((package-archives (list repo)))
+                (package-initialize)
+                (package-install name)))))
+        packages)
+  (package-initialize)
+  (delete-other-windows))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE-SPECIFIC SETTINGS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; LaTeX mode
+(setq latex-run-command "pdflatex")
